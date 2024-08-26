760aa7c02f18ae9e86f374c5139a92f6523cd6f4
ahgr3y <leehh97@gmail.com>
2024-08-26 16:52:48 +0800

    Create window

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while (True):
+        # Activate window's close button
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        # Fill the screen black
+        screen.fill(color=(0,0,0))
+        pygame.display.flip()
     
 # This line ensures the main() function is only called when this file is run directly;
 #  it won't run if it's imported as a module.
